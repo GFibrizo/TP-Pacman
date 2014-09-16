09f3dab233beb01ad1990d8e05c3a60b28f02060
ivan <ivan@ivan-LM13-PC>
2014-09-16 12:20:36 -0300

    GhostState now has a Movement Strategy

--- TP1/src/main/java/com/grupo2/ghost/GhostState.java
+++ TP1/src/main/java/com/grupo2/ghost/GhostState.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.grupo2.ghost;
 
 /**
@@ -11,28 +5,26 @@ package com.grupo2.ghost;
  * @author fibrizo
  */
 public abstract class GhostState {
-    
-    /**
-     * Polymorphicaly calls one of the "move" methods 
-     * implemented by the derivative classes 
-     */
-    public abstract void move();
-    
-    /***************************************************/
-    /***************************************************/
+    protected MovementStrategy movement;
+
+    public GhostState(MovementStrategy movement) {
+        this.movement = movement;
+    }
 
     /**
-     * @return True if the State of the ghost is "Dead".
-     * False otherwise.
+     * Polymorphicaly calls one of the "move" methods implemented by the
+     * derivative classes.
+     */
+    public abstract void move();
+
+    /**
+     * @return True if the State of the ghost is "Dead". False otherwise.
      */
     public abstract boolean isDead();
-    
-    /***************************************************/
-    /***************************************************/
 
     /**
      * @return the next state of the ghost.
      */
     public abstract GhostState returnNextState();
-    
+
 }
---
+++ TP1/src/main/java/com/grupo2/ghost/MovementStrategy.java
@@ -0,0 +1,10 @@
+package com.grupo2.ghost;
+
+/**
+ *
+ * @author ivan
+ */
+public abstract class MovementStrategy {
+    
+    public abstract void move();
+}
