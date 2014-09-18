872cfc10102bd01991d6db9a41d33cad61daa101
fabrizio <zebas.graffe@gmail.com>
2014-09-18 02:34:29 -0300

    Added the ICharacter interface in order to allow to the IGhost's method isBeingEated to receive a ICharacter. The ghost and the pacman will implement ICharacter. Fixed die method of the Ghost class to behave polimorphicaly with the posibles states that can have.

--- TP1/src/main/java/com/grupo2/character/ICharacter.java
+++ TP1/src/main/java/com/grupo2/character/ICharacter.java
@@ -3,28 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package com.grupo2.character;
 
 /**
  *
  * @author fibrizo
  */
-public abstract class Character implements ICharacter{
+public interface ICharacter {
 
-    CollidingArea position;
-
-    protected Character(float x, float y, float radius) {
-        this.position = new CollidingArea(x, y, radius);
-    }
-
-    @Override
     public abstract void move();
 
-    @Override
     public abstract boolean isDead();
 
-    @Override
-    public CollidingArea getPosition() {
-        return position;
-    }
+    public CollidingArea getPosition();
 }
--- TP1/src/main/java/com/grupo2/character/IGhost.java
+++ TP1/src/main/java/com/grupo2/character/IGhost.java
@@ -10,12 +10,16 @@ package com.grupo2.character;
  *
  * @author fibrizo
  */
-public interface IGhost {
+public interface IGhost extends ICharacter {
+    
+    @Override
     public CollidingArea getPosition();
+    @Override
     public void move();
+    @Override
     public boolean isDead();
     public void die();
     public void convertToPrey();
     public void beEaten(IGhost ghost);
-    public boolean isBeingEated(IGhost ghost);
+    public boolean isBeingEated(ICharacter someone);
 }
