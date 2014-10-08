0280fc42e28455b86f2332f57e5939036cda6dd5
ivan <ivan@ivan-LM13-PC>
2014-10-08 00:02:05 -0300

    Pacman constructor fixed according to Character.

--- TP1/src/main/java/com/grupo2/pacman/Pacman.java
+++ TP1/src/main/java/com/grupo2/pacman/Pacman.java
@@ -1,10 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.grupo2.pacman;
+
 import com.grupo2.character.Character;
 import com.grupo2.character.Direction;
 
@@ -13,29 +8,28 @@ import com.grupo2.character.Direction;
  * @author fibrizo
  */
 public class Pacman extends Character {
-    
-    private Direction nextDirection;
-    private boolean alive; 
-    
-    public Pacman() {
-        alive = true;
-    }
 
-    @Override
-    public void move() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	private Direction nextDirection;
+	private boolean alive;
 
-    @Override
-    public boolean isDead() {
-        return alive;
-    }
+	public Pacman(int x, int y) {
+		super(x, y);
+		this.alive = true;
+	}
 
-    @Override
-    public void chooseDirection() {
-       nextDirection = direction;
-    }
-    
-    
-    
+	@Override
+	public void move() {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public boolean isDead() {
+		return alive;
+	}
+
+	@Override
+	public void chooseDirection() {
+		nextDirection = direction;
+	}
+
 }
