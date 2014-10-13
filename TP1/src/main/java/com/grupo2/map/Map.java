14fed037969699c9ef19601ffe5b5ff91bf0ace9
ivan <ivan@ivan-LM13-PC>
2014-10-13 16:46:42 -0300

    Attrs are now private

--- TP1/src/main/java/com/grupo2/map/Map.java
+++ TP1/src/main/java/com/grupo2/map/Map.java
@@ -11,36 +11,37 @@ import java.util.ArrayList;
  * @author mauri
  */
 public class Map {
-    Maze maze;
-    ArrayList<IGhost> ghosts = new ArrayList<>();
-    Pacman thePacman = new Pacman(0,0);
-    private static Map mapSingleton;
-    
-    private Map() {
-       // maze = new Maze( height , width );
+
+	private Maze maze;
+	private ArrayList<IGhost> ghosts = new ArrayList<>();
+	private Pacman thePacman = new Pacman(0, 0);
+	private static Map mapSingleton;
+
+	private Map() {
+		// maze = new Maze( height , width );
 		// this.maze = new MazeXMLBuilder...
-    }
-    
-    public boolean collisionBetween( ICharacter aCharacter , ICharacter anotherCharacter ) {
-        return aCharacter.getPosition().isEqualTo( anotherCharacter.getPosition() );
-    }
-    
-    public boolean collisionWithPacman( ICharacter anotherCharacter ) {
-        return thePacman.getPosition().isEqualTo( anotherCharacter.getPosition() );
-    }
-    
-    public void addGhost(IGhost aCharacterToAdd) {
-        this.ghosts.add(aCharacterToAdd);
-    }
-    
-    public Pacman getPacman() {
-        return this.thePacman;
-    }
-    
-    public static Map getInstance() {
-        if ( mapSingleton == null ) {
-            mapSingleton = new Map();
-        }
-        return mapSingleton;
-        }    
+	}
+
+	public boolean collisionBetween(ICharacter aCharacter, ICharacter anotherCharacter) {
+		return aCharacter.getPosition().isEqualTo(anotherCharacter.getPosition());
+	}
+
+	public boolean collisionWithPacman(ICharacter anotherCharacter) {
+		return thePacman.getPosition().isEqualTo(anotherCharacter.getPosition());
+	}
+
+	public void addGhost(IGhost aCharacterToAdd) {
+		this.ghosts.add(aCharacterToAdd);
+	}
+
+	public Pacman getPacman() {
+		return this.thePacman;
+	}
+
+	public static Map getInstance() {
+		if (mapSingleton == null) {
+			mapSingleton = new Map();
+		}
+		return mapSingleton;
+	}
 }
