827dd7444c0a4b3cf86c7cfba639fc1f182443e7
ivan <ivan@ivan-LM13-PC>
2014-10-13 18:52:06 -0300

    Moved Balls to a new package. Added NullBall

---
+++ TP1/src/main/java/com/grupo2/balls/BigBall.java
@@ -0,0 +1,14 @@
+package com.grupo2.balls;
+
+/**
+ *
+ * @author mauri
+ */
+public class BigBall implements Ball {
+
+    @Override
+    public void isEaten() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
