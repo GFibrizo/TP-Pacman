45719f5b43c49cb7dd85edd2ed824dcf1e86cf3e
Mauricio Bazán <mauriciodbazan@gmail.com>
2014-10-16 02:03:38 -0300

    Refactored Direction class and added Direction Tests

--- TP1/src/main/java/com/grupo2/character/directions/DownDirection.java
+++ TP1/src/main/java/com/grupo2/character/directions/DownDirection.java
@@ -9,16 +9,30 @@ import com.grupo2.interfaces.ICharacter;
  */
 public class DownDirection implements Direction {
 
-	@Override
-	public void stepForward(ICharacter aCharacter) {
-		aCharacter.getPosition().setY(aCharacter.getPosition().getY() - 1);
-	}
-        
-        public boolean sameDirection(Direction anotherD) {
-            return false;
-        }
-        
-        public boolean sameDirection(LeftDirection leftD) {
-            return true;
-        }
+    @Override
+    public void stepForward(ICharacter aCharacter) {
+
+        aCharacter.getPosition().setY(aCharacter.getPosition().getY() - 1);
+    }
+
+    @Override
+    public boolean isEqualTo(LeftDirection aDirection) {
+        return false;
+    }
+
+    @Override
+    public boolean isEqualTo(RightDirection aDirection) {
+        return false;
+    }
+
+    @Override
+    public boolean isEqualTo(UpDirection aDirection) {
+        return false;
+    }
+
+    @Override
+    public boolean isEqualTo(DownDirection aDirection) {
+        return true;
+    }
+    
 }
--- TP1/src/main/java/com/grupo2/character/directions/UpDirection.java
+++ TP1/src/main/java/com/grupo2/character/directions/UpDirection.java
@@ -9,8 +9,28 @@ import com.grupo2.interfaces.ICharacter;
  */
 public class UpDirection implements Direction {
 
-	@Override
-	public void stepForward(ICharacter aCharacter) {
-		aCharacter.getPosition().setY(aCharacter.getPosition().getY() + 1);
-	}
+    @Override
+    public void stepForward(ICharacter aCharacter) {
+        aCharacter.getPosition().setY(aCharacter.getPosition().getY() + 1);
+    }
+    
+    @Override
+    public boolean isEqualTo(LeftDirection aDirection) {
+        return false;
+    }
+
+    @Override
+    public boolean isEqualTo(RightDirection aDirection) {
+        return false;
+    }
+
+    @Override
+    public boolean isEqualTo(UpDirection aDirection) {
+        return true;
+    }
+
+    @Override
+    public boolean isEqualTo(DownDirection aDirection) {
+        return false;
+    }
 }
