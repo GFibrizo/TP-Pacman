cd1f2590e0c468e4d27966b6723a7070f8dc4cff
ivan <ivan@ivan-LM13-PC>
2014-10-23 21:38:34 -0300

    Fixed test to suit the Path constructor

--- TP1/src/test/java/com/grupo2/character/CharacterBuilderTest.java
+++ TP1/src/test/java/com/grupo2/character/CharacterBuilderTest.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.grupo2.character;
 
 import com.grupo2.directions.LeftDirection;
-import com.grupo2.interfaces.IGhost;
 import com.grupo2.pacman.Pacman;
-import java.util.ArrayList;
-import org.junit.AfterClass;
-import static org.junit.Assert.*;
+import java.nio.file.Paths;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import javax.xml.parsers.ParserConfigurationException;
 
 /**
  *
@@ -23,18 +13,18 @@ import javax.xml.parsers.ParserConfigurationException;
  */
 public class CharacterBuilderTest {
 
-    private CharacterBuilder charBuilder;   
-    
-            public CharacterBuilderTest() {
+    private CharacterBuilder charBuilder;
+
+    public CharacterBuilderTest() {
     }
-    
+
     @Before
     public void setUpClass() {
-            // CHANGE THE PATH TO YOURS
         try {
-            charBuilder = new CharacterXMLBuilder("/home/fibrizo/Escritorio/TP-Pacman/tp/TP1/src/main/resources/laberintos/PersonajesSimple.xml"); 
-        } catch (Exception e) {
-            assert(false);
+            charBuilder = new CharacterXMLBuilder(Paths.get("src", "main", "resources", "laberintos", "PersonajesSimple.xml"));
+        }
+        catch (Exception e) {
+            assert (false);
         }
     }
 
@@ -43,25 +33,26 @@ public class CharacterBuilderTest {
      */
     @Test
     public void getPacmanPosition() {
-        
+
         Pacman builtPacman;
         try {
             builtPacman = charBuilder.getPacman();
-        } catch (Exception e) {
-            assert(false);
+        }
+        catch (Exception e) {
+            assert (false);
             return;
         }
 
         Coordinate builtPacPosition = builtPacman.getPosition();
         Direction builtPacDirection = builtPacman.getDirection();
-        
-        Coordinate expectedPacPosition = new Coordinate(4,9);
+
+        Coordinate expectedPacPosition = new Coordinate(4, 9);
         LeftDirection expectedPacDirection = new LeftDirection();
-        
+
         boolean samePositions = builtPacPosition.isEqualTo(expectedPacPosition);
         boolean sameDirections = builtPacDirection.isEqualTo(expectedPacDirection);
-        
+
         assertTrue(sameDirections && samePositions);
     }
-    
+
 }
--- TP1/src/test/java/com/grupo2/map/MapTest.java
+++ TP1/src/test/java/com/grupo2/map/MapTest.java
@@ -1,13 +1,11 @@
 package com.grupo2.map;
 
-
-import com.grupo2.directions.DownDirection;
-
 import com.grupo2.character.CharacterXMLBuilder;
-
+import com.grupo2.directions.DownDirection;
 import com.grupo2.ghost.Ghost;
 import com.grupo2.interfaces.ICharacter;
 import com.grupo2.maze.MazeXMLBuilder;
+import java.nio.file.Paths;
 import org.junit.AfterClass;
 import static org.junit.Assert.assertEquals;
 import org.junit.BeforeClass;
@@ -43,9 +41,11 @@ public class MapTest {
         other.setDirection(new DownDirection());
         Map map;
         try {
-            map = new Map(new MazeXMLBuilder("path"), new CharacterXMLBuilder("otherpath"));
-        } catch (Exception e){
-            assert(false);
+            map = new Map(new MazeXMLBuilder(Paths.get("src", "main", "resources", "laberintosprueba", "Laberinto.xml")),
+                    new CharacterXMLBuilder(Paths.get("src", "main", "resources", "laberintos", "PersonajesSimple.xml")));
+        }
+        catch (Exception e) {
+            assert (false);
             return;
         }
         boolean expResult = true;
