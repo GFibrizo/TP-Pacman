407a2e6282458e74a9204bf62c55ce485e02c2fe
Mauricio Bazán <mauriciodbazan@gmail.com>
2014-11-08 23:52:21 -0300

    Created Fruit class test

---
+++ TP1/src/test/java/com/grupo2/fruit/FruitTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.grupo2.fruit;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mauri
+ */
+public class FruitTest {
+    
+    public FruitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
