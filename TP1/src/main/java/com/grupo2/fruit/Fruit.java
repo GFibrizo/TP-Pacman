4fa897d0df5f071e8a8776d56230fd410df4bc2b
Mauricio Bazán <mauriciodbazan@gmail.com>
2014-11-08 23:50:35 -0300

    Created Fruit class

---
+++ TP1/src/main/java/com/grupo2/fruit/Fruit.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.grupo2.fruit;
+
+import com.grupo2.board.Board;
+import com.grupo2.character.Character;
+/**
+ *
+ * @author mauri
+ */
+public abstract class Fruit extends Character {
+    public Fruit() {
+        super();
+    }
+}
