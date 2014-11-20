d2675ca4638a473359e99dcadfc3352d6886e7fc
ivan <ivan@ivan-LM13-PC>
2014-11-20 18:16:51 -0300

    Pacman sprites and seeker strategy

--- TP1/src/main/java/com/grupo2/graphicView/PacmanView.java
+++ TP1/src/main/java/com/grupo2/graphicView/PacmanView.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.grupo2.graphicView;
 
 import com.grupo2.constants.ViewConstants;
-import com.grupo2.directions.DownDirection;
-import com.grupo2.directions.LeftDirection;
-import com.grupo2.directions.RightDirection;
-import com.grupo2.directions.UpDirection;
 import com.grupo2.pacman.Pacman;
 import com.grupo2.view.ObjectView;
 import java.awt.Dimension;
+import java.nio.file.Paths;
 import javax.swing.ImageIcon;
 
 /**
@@ -21,52 +13,48 @@ import javax.swing.ImageIcon;
  */
 public class PacmanView extends ObjectView {
 
-    private Pacman object;
-    private int count;
+	private Pacman object;
+	private int count;
 
-    public PacmanView(Pacman pacman) {
-        this.object = pacman;
-        this.x = object.getPosition().getX();
-        this.y = object.getPosition().getY();
-        ImageIcon boardBackground = new ImageIcon(ViewConstants.PACMAN_RIGHT);
-        this.image = boardBackground.getImage();
-        setPreferredSize(new Dimension(width, height));
-        this.count = 1;
-    }
+	public PacmanView(Pacman pacman) {
+		this.object = pacman;
+		this.x = object.getPosition().getX();
+		this.y = object.getPosition().getY();
+		ImageIcon boardBackground = new ImageIcon(ViewConstants.PACMAN_RIGHT);
+		this.image = boardBackground.getImage();
+		setPreferredSize(new Dimension(width, height));
+		this.count = 1;
+	}
 
-    private ImageIcon chooseImage() {
-        directionString = object.getDirection().toString;
-        /*if (object.getDirection().EqualTo(new LeftDirection())) {
-            return new ImageIcon(Paths.get("src", "main", "graphicResources", directionString + count ".png").toString());
-        } else if (object.getDirection().EqualTo(new RightDirection())) {
-            return new ImageIcon(ViewConstants.PACMAN_RIGHT+count);
-        } else if (object.getDirection().EqualTo(new UpDirection())) {
-            return new ImageIcon(ViewConstants.PACMAN_UP);
-        } else if (object.getDirection().EqualTo(new DownDirection())) {
-            return new ImageIcon(ViewConstants.PACMAN_DOWN);
-        } else {
-            return null;
-        }*/
-        this.changeSpriteNumber();
-        return new ImageIcon(Paths.get("src", "main", "graphicResources", directionString + count ".png").toString());
-    }
+	private ImageIcon chooseImage() {
+		String directionString = object.getDirection().toString();
+		/*if (object.getDirection().EqualTo(new LeftDirection())) {
+		 return new ImageIcon(Paths.get("src", "main", "graphicResources", directionString + count ".png").toString());
+		 } else if (object.getDirection().EqualTo(new RightDirection())) {
+		 return new ImageIcon(ViewConstants.PACMAN_RIGHT+count);
+		 } else if (object.getDirection().EqualTo(new UpDirection())) {
+		 return new ImageIcon(ViewConstants.PACMAN_UP);
+		 } else if (object.getDirection().EqualTo(new DownDirection())) {
+		 return new ImageIcon(ViewConstants.PACMAN_DOWN);
+		 } else {
+		 return null;
+		 }*/
+		this.changeSpriteNumber();
+		return new ImageIcon(Paths.get("src", "main", "graphicResources", "Pacman" + directionString + String.valueOf(count) + ".png").toString());
+	}
 
-    @Override
-    public void update() {
-        this.x = object.getPosition().getX();
-        this.y = object.getPosition().getY();
-        ImageIcon boardBackground = chooseImage();
-        if (boardBackground != null) {
-            this.image = boardBackground.getImage();
-        }
-    }
+	@Override
+	public void update() {
+		this.x = object.getPosition().getX();
+		this.y = object.getPosition().getY();
+		ImageIcon boardBackground = chooseImage();
+		if (boardBackground != null) {
+			this.image = boardBackground.getImage();
+		}
+	}
 
-    private void changeSpriteNumber() {
-        if (this.count == 0) {
-            this.count++;
-        } else {
-            this.count = 0;
-        }
-    }
+	private void changeSpriteNumber() {
+		count = (count + 1) % 2;
+	}
 
 }
