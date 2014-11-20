d08a6d53157ae3f4ab68f8d0916f0196429d835e
ivan <ivan@ivan-LM13-PC>
2014-11-19 23:27:36 -0300

    Initial try to make Game iterate through levels

--- TP1/src/main/java/com/grupo2/game/Game.java
+++ TP1/src/main/java/com/grupo2/game/Game.java
@@ -1,8 +1,11 @@
 package com.grupo2.game;
 
 import com.grupo2.controller.Controller;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.logging.Logger;
 import javax.xml.parsers.ParserConfigurationException;
 
 /**
@@ -11,48 +14,56 @@ import javax.xml.parsers.ParserConfigurationException;
  */
 public class Game {
 
-    private Controller controller;
-    private Level actualLevel;
-    private Integer levelIndex;
+	private Controller controller;
+	private Level actualLevel;
+	private int levelIndex;
+	private int qtyLevels;
 
-    public Game(Controller controller) {
-        this.controller = controller;
-        levelIndex = 0;
-    }
+	public Game(Controller controller) {
+		this.controller = controller;
+		this.levelIndex = 0;
+		this.qtyLevels = 0;
+		try {
+			Files.walk(Paths.get("src", "main", "resources", "Levels")).forEachOrdered(levelFile -> {
+				if (levelFile.toString().matches(".*MazeLevel[0-9]+.xml")) {
+					this.qtyLevels++;
+				}
+			});
+		} catch (IOException ex) {
+			Logger.getLogger(Game.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		}
+	}
 
-    private void startLevel() throws ParserConfigurationException {
-        actualLevel = getNextLevel(levelIndex.toString());
-    }
+	private void startLevel() throws ParserConfigurationException {
+		this.actualLevel = getNextLevel();
+		this.levelIndex = (this.levelIndex + 1) % this.qtyLevels;
+	}
 
-    private Level getNextLevel(String index) throws ParserConfigurationException {
-        Path mazePath = Paths.get("src", "main", "resources", "laberintos", "LaberintoSimple.xml");
-        Path characterPath = Paths.get("src", "main", "resources", "laberintos", "PersonajesSimple.xml");
+	private Level getNextLevel() throws ParserConfigurationException {
+		Path mazePath = Paths.get("src", "main", "resources", "Levels", "MazeLevel" + String.valueOf(this.levelIndex) + ".xml");
+		Path characterPath = Paths.get("src", "main", "resources", "Levels", "CharactersLevel" + String.valueOf(this.levelIndex) + ".xml");
+		return new Level(mazePath, characterPath, controller);
+	}
 
-        //Path mazePath = Paths.get("src", "main", "resources", "Levels", "Level"+ index, "Maze.xml");
-        //Path characterPath = Paths.get("src", "main", "resources", "Levels", "Level"+ index, "Characters.xml");
-        return new Level(mazePath, characterPath, controller);
-    }
-
-    public void play() {
-        boolean keepPlaying = true;
-        int totalScore = 0;
-        try {
-            this.startLevel();
-        } catch (ParserConfigurationException ex) {
-            keepPlaying = false;
-        }
-        while (keepPlaying) {
-            int score = actualLevel.play();
-            //if (!showContinueToNextLevel(score)) break;
-            levelIndex++;
-            try {
-                this.startLevel();
-            } catch (ParserConfigurationException ex) {
-                keepPlaying = false;
-            }
-            totalScore += score;
-        }
-        //showEndGame(totalScore);
-    }
+	public void play() {
+		boolean keepPlaying = true;
+		int totalScore = 0;
+		try {
+			this.startLevel();
+		} catch (ParserConfigurationException ex) {
+			keepPlaying = false;
+		}
+		while (keepPlaying) {
+			int score = this.actualLevel.play();
+			//if (!showContinueToNextLevel(score)) break;
+			try {
+				this.startLevel();
+			} catch (ParserConfigurationException ex) {
+				keepPlaying = false;
+			}
+			totalScore += score;
+		}
+		//showEndGame(totalScore);
+	}
 
 }
