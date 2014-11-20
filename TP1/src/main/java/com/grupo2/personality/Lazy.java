eab5280cf91b14ab1b03022d84923ede012bbb58
Familia <Familia@Familia-PC>
2014-11-20 18:18:37 -0300

    Implemented other personalities. Candidates for refactor.

--- TP1/src/main/java/com/grupo2/personality/Lazy.java
+++ TP1/src/main/java/com/grupo2/personality/Lazy.java
@@ -7,8 +7,13 @@ package com.grupo2.personality;
 
 import com.grupo2.cell.Cell;
 import com.grupo2.character.Direction;
+import com.grupo2.command.HunterStartsChaseOfPacman;
 import com.grupo2.constants.Constants;
+import com.grupo2.eventHandling.Subscriber;
 import com.grupo2.ghostState.Personality;
+import com.grupo2.maze.MazePublisher;
+import com.grupo2.movementStrategies.ChaseStrategy;
+import com.grupo2.movementStrategies.RandomStrategy;
 import java.util.Map;
 
 /**
@@ -19,21 +24,24 @@ public class Lazy extends Personality {
 
     public Lazy() {
         vision = Constants.VISION2;
+        Subscriber sub = new HunterStartsChaseOfPacman(this);
+        MazePublisher.getInstance().subscribe(MazePublisher.MazeEvent.GHOSTISCLOSETOPACMAN, sub);
+        movement = new RandomStrategy();
     }
 
     @Override
     public Direction getNewDirection(Map<Direction, Cell> allowedDirections) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return movement.getNewDirection(allowedDirections);
     }
 
     @Override
     public void beginPacmanChase() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.movement = new ChaseStrategy();
     }
 
     @Override
     public void stopPacmanChase() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.movement = new RandomStrategy();
     }
 
     @Override
